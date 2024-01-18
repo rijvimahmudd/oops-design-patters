2410e77ef7b2d9841224399abcd8e5bab79f3485
rijvimahmudd <93908058+rijvimahmudd@users.noreply.github.com>
2024-01-18 16:27:31 +0600

    work on factory method pattern

---
+++ Creational/Factory Method/documentProcessingSystem.cpp
@@ -0,0 +1,12 @@
+/**
+ * Design a document processing system that supports different document types (e.g., PDF, Word, Excel). Apply the Factory Method pattern to create document objects. Each document type should have a unique method, such as generateReport() for PDF and createSpreadsheet() for Excel.
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
